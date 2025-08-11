81bf7e32e139d226d0e097b3a5715f2aec52ee9a
Raiyan Hasan <raiyanhasan2006@gmail.com>
2025-08-11 06:37:16 +0530

    feat: add user roles and task status constants
    This commit introduces constants for user roles and task statuses in `src/utils/constants.js`.
    
    - `UserRolesEnum`: Defines roles for 'admin', 'project_admin', and 'member'.
    - `TasksStatusEnum`: Defines statuses for 'todo', 'in_progress', and 'done'.
    
    These constants will be used throughout the application to ensure consistency.

---
+++ src/utils/ApiError.js
@@ -0,0 +1,16 @@
+class ApiError extends Error {
+  constructor(message="Something went wrong", statusCode, errors=[], stack="") {
+    super(message);
+    this.message = message;
+    this.data = null;
+    this.success = false;
+    this.errors = errors;
+    this.statusCode = statusCode;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }   
+  }
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const UserRolesEnum = {
+    ADMIN: "admin",
+    PROJECT_ADMIN: "project_admin",
+    MEMBER: "member"
+};
+
+export const AvailableUserRoles = Object.values(UserRolesEnum);
+
+export const  TasksStatusEnum = {
+    TODO: "todo",
+    IN_PROGRESS: "in_progress",
+    DONE: "done"    
+};
+export const AvailableTasksStatus = Object.values(TasksStatusEnum);
